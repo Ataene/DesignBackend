061d8206c215d99e101f63110da3a53975122c08
Emmanuel Alafonye <ke.alafonye@yahoo.com>
2022-09-26 17:19:04 -0600

    db connected

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,22 +1,21 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
 
-const connectionURL = process.env.connectionURL;
 
-async function connect() {
-  try {
-    await mongoose.connect(
-      connectionURL,
-      { useNewUrlParser: true, useUnifiedTopology: true }, 
-      () => {
-        console.log("Connected to MongoDB");
-      }, 6000000
-    );
-  } catch (error) {
-    console.log("Error connecting to Atlas");
-  }
-}
 
-connect();
 
-module.exports = mongoose;
+// async function connect() {
+//   try {
+//     await mongoose.connect(
+//       connectionURL,
+//       { useNewUrlParser: true, useUnifiedTopology: true }, 
+//       () => {
+//         console.log("Connected to MongoDB");
+//       }
+//     );
+//   } catch (error) {
+//     console.log("Error connecting to Atlas");
+//   }
+// }
+
+// connect();
+
+// module.exports = mongoose;
